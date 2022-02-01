10940612846572486c2f32efd365c2fb0d720aa4
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-02-01 22:01:59 +0530

    zigzag

---
+++ Trees/ZigZagTreeTraversal.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class ZigZagTreeTraversal {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int l = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+            if (!level.isEmpty()) {
+                if (l % 2 == 1) {
+                    Collections.reverse(level);
+                }
+                ret.add(level);
+            }
+            l++;
+        }
+        return ret;
+    }
+
+}
