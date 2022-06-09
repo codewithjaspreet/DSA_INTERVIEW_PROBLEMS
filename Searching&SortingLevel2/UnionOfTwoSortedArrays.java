bc83d7e212596627d10fe05ad357a9195caf9286
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-06-09 12:36:19 +0530

    Union of two sorted arrays - gfg

--- Searching&SortingLevel2/UnionOfTwoSortedArrays.java
+++ Searching&SortingLevel2/UnionOfTwoSortedArrays.java
@@ -1,38 +1,111 @@
-// brute force - hashset  nlogn
-
 class Solution
 {
+    // O(N+M) - optimised
     //Function to return a list containing the union of the two arrays.
     public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        // add your code 
-        // int[] temp = mergeTwoSortedArrays(arr1 , arr2, n , m);
-        HashSet<Integer> map = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         
-        for(int a : arr1) {
-            map.add(a);
+        int i = 0 ; int j = 0;
+        
+       while (i < n && j < m) {
+      if (arr1[i] == arr2[j]) {
+
+        if (ans.size() > 0 && ans.get(ans.size() - 1) != arr1[i]) {
+          ans.add(arr1[i]);
         }
+
+        else if (ans.size() == 0) {
+          ans.add(arr1[i]);
+        }
+
+        i++; j++;
+      }
+      else if (arr1[i] < arr2[j]) {
+
+        if (ans.size() > 0 && ans.get(ans.size() - 1) != arr1[i]) {
+          ans.add(arr1[i]);
+
+        }
+        else if (ans.size() == 0) {
+          ans.add(arr1[i]);
+        }
+
+        i++;
+      }
+      else {
+          
+        if (ans.size() > 0 && ans.get(ans.size() - 1) != arr2[j]) {
+          ans.add(arr2[j]);
+
+        }
+
+        else if (ans.size() == 0) {
+          ans.add(arr2[j]);
+        }
+
+        j++;
+
+      }
+    }
+
+    while (i < n) {
+      if (ans.get(ans.size() - 1) != arr1[i]) {
+        ans.add(arr1[i]);
+      }
+      i++;
+    }
+
+    while (j < m) {
+      if (ans.get(ans.size() - 1) != arr2[j]) {
+        ans.add(arr2[j]);
+      }
+      j++;
+    }
+
+    return ans;
+}
+}
+
+
+
+
+
+// // brute force - hashset  nlogn
+
+// class Solution
+// {
+//     //Function to return a list containing the union of the two arrays.
+//     public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+//     {
+//         // add your code 
+//         // int[] temp = mergeTwoSortedArrays(arr1 , arr2, n , m);
+//         HashSet<Integer> map = new HashSet<>();
+        
+//         for(int a : arr1) {
+//             map.add(a);
+//         }
     
         
-        for(int a: arr2){
-            map.add(a);
-        }
-        ArrayList<Integer> f = new ArrayList<>(map.size());
-        int[] ans = new int[map.size()]; int i = 0;
-        for(int a : map){
-            ans[i]  = a;
-            i++;
-        }
+//         for(int a: arr2){
+//             map.add(a);
+//         }
+//         ArrayList<Integer> f = new ArrayList<>(map.size());
+//         int[] ans = new int[map.size()]; int i = 0;
+//         for(int a : map){
+//             ans[i]  = a;
+//             i++;
+//         }
         
-        Arrays.sort(ans);
+//         Arrays.sort(ans);
         
-        for(int a : ans){
-            f.add(a);
-        }
+//         for(int a : ans){
+//             f.add(a);
+//         }
         
-        return f;
+//         return f;
         
-    }
+//     }
     
  
-}
+// }
