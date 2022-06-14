98af280c866d76413af78512e6829f7a03a6ed1b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-06-14 09:51:00 +0530

    Counts Zeros Xor Pairs

---
+++ Searching&SortingLevel2/Count xor zeroes pair.java
@@ -0,0 +1,32 @@
+
+// time and space - O(N)
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        //Complete the function
+        long ans = 0;
+        
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int a : arr){
+            
+            if(map.containsKey(a)) map.put( a , map.get(a) + 1);
+            else map.put(a , 1);
+            
+        }
+        
+        for(int a  : map.keySet()){
+            
+            int val = map.get(a);
+            ans+= ((val-1)*val)/2;
+        }
+        return ans;
+    }
+    
+    
+}
+
+
