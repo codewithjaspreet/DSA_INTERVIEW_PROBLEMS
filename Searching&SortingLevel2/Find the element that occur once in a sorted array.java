b790fd3ad48591a8226c9d8d1daf62c66571f84b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-06-15 20:21:21 +0530

    tricky optimised - well commented approach 😎

--- Searching&SortingLevel2/Find the element that occur once in a sorted array.java
+++ Searching&SortingLevel2/Find the element that occur once in a sorted array.java
@@ -25,3 +25,63 @@ public static int search(int A[], int N)
     }
 
     // optimised logn binary search approach ->
+
+
+    // the idea is to move in that side where odd no of people lie
+    class Sol {
+    public static int search(int A[], int N)
+    {
+        // your code here
+        int low = 0 ; int high= N-1;
+        int ans = -1;
+        
+        // safety checks for fisrt element  , last element and if there is only 1 element
+        if(A.length == 1) return A[0];
+        if(A[0] != A[1]) return A[0];
+        if(A[N-1] !=A[N-2]) return A[N-1];
+        
+       while(low <=high){
+           
+           int mid = (low + high)/2;
+
+           // if left and right side both are not equa; then the ans is this as it is single element
+
+
+           if(A[mid] != A[mid-1] && A[mid] != A[mid+1] ){
+             
+               return A[mid];
+           }
+
+           else if(A[mid] == A[mid-1]){
+               
+               int count = (mid - low + 1);
+               
+               // low to mid how many numbers are there
+
+               if(count %2 == 0 ) low = mid + 1;
+
+                // if even then that single element will wont be in this area
+               else high = mid - 2; 
+               
+               // if not even than this ,  and ** put directly to mid -2 as we are done with mid - 1
+           }
+           else if(A[mid] == A[mid+1]){
+               
+               int count = (high - mid + 1); // high to mid how many numbers are there
+
+               if(count % 2 == 0) high = mid - 1;
+               
+               // if even then that single element will wont be in this area
+               
+               else low = mid +2; 
+
+               // else this as we are already done with mid + 1 check;
+
+           }
+       }
+       
+       return -1;
+    }
+    
+
+}
