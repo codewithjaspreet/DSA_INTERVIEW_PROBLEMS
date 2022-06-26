6b142959fea84167c3039a457be5ea4951756d61
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-06-26 23:22:40 +0530

    Allocation concept

--- Searching&SortingLevel2/Capacity To Ship Packages Within D Days.java
+++ Searching&SortingLevel2/Capacity To Ship Packages Within D Days.java
@@ -2,51 +2,56 @@
 
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-          
-        int max = Integer.MIN_VALUE; int sum = 0 ;
-        
-        for(int  a : weights ){
-            max=Math.max(a,max);
-            sum+=a;
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int a : weights) {
+            max = Math.max(a, max);
+            sum += a;
         }
-        
-        int low = max; int high  = sum; int ans = 0;
-        
-        while(low <= high){
-            
-            int mid = low + (high - low) /2;
-            
-            if(isPossible(weights, mid, days)){
-                
-                ans = mid ;
+
+        int low = max;
+        int high = sum;
+        int ans = 0;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (isPossible(weights, mid, days)) {
+
+                ans = mid;
                 high = mid - 1;
             }
-            
+
             else {
                 low = mid + 1;
             }
-        
+
         }
-        
+
         return ans;
     }
-    
-    public static boolean isPossible(int[] weights , int mid , int days){
-        
-        int sum = 0 ; int count = 1;
-        
-        for(int a  : weights){
-            
-            sum+=a;
-            
-            if(sum  > mid){
-               
+
+    public static boolean isPossible(int[] weights, int mid, int days) {
+
+        int sum = 0;
+        int count = 1;
+
+        for (int a : weights) {
+
+            sum += a;
+
+            if (sum > mid) {
+
                 count++;
                 sum = a;
-                
+
             }
         }
-        
+
         return count <= days;
     }
+
 }
