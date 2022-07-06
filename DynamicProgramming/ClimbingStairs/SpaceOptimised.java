1fcb8de33460ea689d88c4ca9fd9a8cf551110aa
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-06 07:23:02 +0530

    final one space optimised

---
+++ DynamicProgramming/ClimbingStairs/SpaceOptimised.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        // base cases
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for (int i = 2; i < n; i++) {
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
+}
