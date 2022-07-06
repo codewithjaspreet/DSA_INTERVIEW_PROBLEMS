c4d4a5c329f1323f77d37a1c7dda624fda699cdf
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-06 07:23:10 +0530

    tabulation

---
+++ DynamicProgramming/ClimbingStairs/Tabulation.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+        }
+        return dp[n];
+
+    }
+
+}
