59b2bda1ef32174a0614492057b54930f5a81438
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-09 10:07:36 +0530

    O(N*4) time & O(4) space - the best

---
+++ DynamicProgramming/NinjaTraning/SpaceOptimisation.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int ninjaTraining(int n, int[][] points) {
+
+        int prev[] = new int[4];
+
+        prev[0] = Math.max(points[0][1], points[0][2]);
+        prev[1] = Math.max(points[0][0], points[0][2]);
+        prev[2] = Math.max(points[0][0], points[0][1]);
+        prev[3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
+
+        for (int day = 1; day < n; day++) {
+
+            int temp[] = new int[4];
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) {
+                        temp[last] = Math.max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+
+            prev = temp;
+
+        }
+
+        return prev[3];
+    }
+}
