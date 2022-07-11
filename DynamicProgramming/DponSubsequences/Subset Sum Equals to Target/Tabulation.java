537b23a26a2745cd360168e95a7486c9aa99d6fa
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-11 12:59:55 +0530

    stack space removed

---
+++ DynamicProgramming/DponSubsequences/Subset Sum Equals to Target/Tabulation.java
@@ -0,0 +1,37 @@
+// Time Complexity: O(N*K)
+
+// Reason: There are two nested loops
+
+// Space Complexity: O(N*K)
+
+// Reason: We are using an external array of size ‘N*K’. Stack Space is eliminated.
+
+public class Solution {
+    public static boolean subsetSumToK(int n, int k, int arr[]) {
+        // Write your code here.
+
+        boolean dp[][] = new boolean[n][k + 1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        if (arr[0] <= k)
+            dp[0][arr[0]] = true;
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+
+                boolean notTaken = dp[ind - 1][target];
+
+                boolean taken = false;
+                if (arr[ind] <= target)
+                    taken = dp[ind - 1][target - arr[ind]];
+
+                dp[ind][target] = notTaken || taken;
+            }
+        }
+
+        return dp[n - 1][k];
+    }
+}
