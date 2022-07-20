3409ee41b1264488561008bc84bdbbdeb5b0f305
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-20 20:59:41 +0530

    Inorder  ->  LEFT ROOT RIGHT

---
+++ Trees/InOrderTraversal.java
@@ -0,0 +1,23 @@
+res.add(root.data);
+class Tree {
+    // Function to return a list containing the postorder traversal of the tree.
+    ArrayList<Integer> postOrder(Node root) {
+        // Your code goes here
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        helper(root, arr);
+
+        return arr;
+    }
+
+    public static void helper(Node root, ArrayList<Integer> res) {
+
+        if (root != null) {
+
+           
+            helper(root.left, res);
+            res.add(root.data);
+            helper(root.right, res);
+        }
+    }
+}
