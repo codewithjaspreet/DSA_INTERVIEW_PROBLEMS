82a99e2a4157f60192a03e85fbeb246455c2904b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-20 21:00:17 +0530

    PreOrder -> ROOT LEFT RIGHT

---
+++ Trees/PreOrderTraversal.java
@@ -0,0 +1,21 @@
+class Tree {
+    // Function to return a list containing the postorder traversal of the tree.
+    ArrayList<Integer> postOrder(Node root) {
+        // Your code goes here
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        helper(root, arr);
+
+        return arr;
+    }
+
+    public static void helper(Node root, ArrayList<Integer> res) {
+
+        if (root != null) {
+
+            res.add(root.data);
+            helper(root.left, res);
+            helper(root.right, res);
+        }
+    }
+}
