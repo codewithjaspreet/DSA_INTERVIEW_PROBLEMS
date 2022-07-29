7c7490f36311df278fceed046d66791f14e6905b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-07-29 09:01:43 +0530

    level order with a bool flag

--- Trees/ZigZagLevelOrderTraversal.java
+++ Trees/ZigZagLevelOrderTraversal.java
@@ -1,6 +1,8 @@
 
 // flag variable - >  alter on each level
 
+// gfg : 
+
 class GFG {
     // Function to store the zig zag order traversal of tree in a list.
     ArrayList<Integer> zigZagTraversal(Node root) {
@@ -41,5 +43,41 @@ class GFG {
         }
 
         return ans;
+    }
+}
+
+// leetcode :
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> op = new LinkedList<>();
+        if (root == null) {
+            return op;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean odd = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> res = new LinkedList<>();
+            for (int i = 1; i <= size; i++) {
+                TreeNode node = queue.poll();
+                if (odd) {
+                    res.add(node.val);
+                } else {
+                    res.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+            }
+            op.add(res);
+            odd = !odd;
+        }
+        return op;
     }
 }
