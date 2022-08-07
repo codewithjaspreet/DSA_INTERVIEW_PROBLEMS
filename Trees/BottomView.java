db5148ad7058f271d7c373e87b75d5f489534c69
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-08-07 17:34:05 +0530

    bottom view - hasmap- if condition rem

---
+++ Trees/BottomView.java
@@ -0,0 +1,57 @@
+
+// bottom view , pair class  concept + level order
+// hashmap  implemented
+
+class Solution {
+
+    static class Pair {
+
+        int hd;
+        Node node;
+
+        Pair(int hd, Node node) {
+
+            this.hd = hd;
+            this.node = node;
+
+        }
+
+    }
+
+    // Function to return a list containing the bottom view of the given tree.
+    public ArrayList<Integer> bottomView(Node root) {
+        // Code here
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(0, root));
+
+        while (!q.isEmpty()) {
+
+            Pair r = q.poll();
+
+            map.put(r.hd, r.node.data);
+
+            if (r.node.left != null) {
+
+                q.add(new Pair(r.hd - 1, r.node.left));
+
+            }
+
+            if (r.node.right != null) {
+
+                q.add(new Pair(r.hd + 1, r.node.right));
+
+            }
+
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+
+        return ans;
+
+    }
+}
