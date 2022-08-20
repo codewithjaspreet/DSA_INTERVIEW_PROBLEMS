ad0ce420520d070f19fe73083c53ad7db7ff4f8b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-08-20 18:35:49 +0530

    inorder of BST is sorted

--- Trees/BinarySearchTrees/KthSmallestInABst.java
+++ Trees/BinarySearchTrees/KthSmallestInABst.java
@@ -34,3 +34,35 @@ class Solution {
 
     }
 }
+
+// optimsed
+// time - o(n)
+// space - o(1)
+//
+class Solution {
+
+    int count = 0;
+    int result = Integer.MIN_VALUE;
+
+    public int kthSmallest(TreeNode root, int k) {
+        Inorder(root, k);
+        return result;
+    }
+
+    /* For kth smallest element */
+    void Inorder(TreeNode root, int k) {
+        if (root == null)
+            return;
+        Inorder(root.left, k);
+        count++;
+        if (count == k)
+            result = root.val;
+        /*
+         * NO need to visit the right tree if count is already greater than k, if
+         * count>k that means we already found K
+         */
+        if (count < k)
+            Inorder(root.right, k);
+    }
+
+}
