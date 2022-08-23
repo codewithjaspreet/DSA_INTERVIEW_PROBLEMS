262699a8c7ce9a12a45637ee038e05ec85d5522a
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-08-23 14:58:47 +0530

    imp snippet

---
+++ BasicMaths/isPalindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String is_palindrome(int n) {
+        // Code here
+
+        StringBuilder sb = new StringBuilder();
+
+        String x1 = Integer.toString(n);
+
+        for (int i = x1.length() - 1; i >= 0; i--) {
+
+            sb.append(x1.charAt(i));
+        }
+
+        return x1.equals(sb.toString()) ? "Yes" : "No";
+    }
+}
