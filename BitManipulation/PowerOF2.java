b990856bba60b2c7ede18db6aabf375a40ad4fd0
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-08-28 14:04:15 +0530

    n > 0 && (n & n - 1) == 0

---
+++ BitManipulation/PowerOF2.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n) {
+
+        // Your code here
+
+        return n > 0 && (n & n - 1) == 0;
+
+    }
+
+}
