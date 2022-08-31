06c179dd4f3ee86e74766a15877997c974be6a57
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-08-31 15:56:21 +0530

    two queues and single queue both added

--- Stacks/StackUsingQueue.java
+++ Stacks/StackUsingQueue.java
@@ -66,6 +66,75 @@ class MyStack {
     }
 }
 
+// using single queue
+
+class MyStack {
+
+    Queue<Integer> mainQ;
+
+    public MyStack() {
+
+        mainQ = new ArrayDeque<>();
+
+    }
+
+    public void push(int x) {
+
+        int size = mainQ.size();
+
+        mainQ.add(x);
+
+        for (int i = 0; i < size; i++) {
+
+            int rem = mainQ.remove();
+            mainQ.add(rem);
+        }
+
+    }
+
+    public int pop() {
+
+        if (!empty()) {
+
+            return mainQ.poll();
+        }
+
+        else {
+
+            return -1;
+        }
+
+    }
+
+    public int top() {
+
+        if (!empty()) {
+
+            return mainQ.peek();
+        }
+
+        else {
+
+            return -1;
+        }
+
+    }
+
+    public boolean empty() {
+
+        return mainQ.isEmpty() ? true : false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
