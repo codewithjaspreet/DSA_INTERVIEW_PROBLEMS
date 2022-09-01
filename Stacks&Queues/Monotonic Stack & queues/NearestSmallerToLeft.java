3fe7a28350a0f4d2ac6d61a955b77a27c3f06c32
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-01 12:15:15 +0530

    smaller to left- forward pass

---
+++ Stacks&Queues/Monotonic Stack & queues/NearestSmallerToLeft.java
@@ -0,0 +1,46 @@
+
+// o(n) time and o(n) space
+// using stack
+// next greater to left
+// forward traversal
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static int[] nearestSmallerToLeft(int[] arr, int n) {
+
+        int[] nsl = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < ngl.length; i++) {
+
+            if (st.size() == 0) {
+                ngl[i] = -1;
+            }
+
+            else if (st.size() > 0 && st.peek() < arr[i]) {
+                ngl[i] = st.peek();
+            }
+
+            else if (st.size() > 0 && st.peek() >= arr[i]) {
+                while (st.size() > 0 && st.peek() >= arr[i]) {
+                    st.pop();
+                }
+
+                if (st.size() == 0) {
+                    ngl[i] = -1;
+                }
+
+                else {
+                    ngl[i] = st.peek();
+                }
+            }
+
+            st.push(arr[i]);
+        }
+
+        return ngl;
+    }
+}
