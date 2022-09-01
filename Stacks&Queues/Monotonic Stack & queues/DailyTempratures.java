72fdf519dd0222cc27fb07bbf866a88e4afb8809
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-01 16:50:23 +0530

    index store + next greater on right variation

---
+++ Stacks&Queues/Monotonic Stack & queues/DailyTempratures.java
@@ -0,0 +1,27 @@
+
+// next greater to right using stack
+// store the index of the element in the stack
+
+class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int[] ret = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+
+            }
+
+            stack.push(i);
+        }
+
+        return ret;
+
+    }
+}
