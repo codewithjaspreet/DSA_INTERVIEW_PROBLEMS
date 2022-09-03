c5b1cea83c68d5f7fbf7a555a53428092ef19601
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-03 17:54:15 +0530

    sort - nlogn  + o(1) space

---
+++ GreedyAlgorithms/N Meetings One Room.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public static boolean meetingRooms(int intervals[][]) {
+        // write code
+
+        // sort the 2d array on the basis of the start time
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int ep = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] < ep) {
+
+                return false;
+
+            }
+
+            else {
+
+                ep = intervals[i][1];
+            }
+        }
+
+        return true;
+
+    }
+}
