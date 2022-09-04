477d35fa7f705d38579fe64458e979e81deefb2c
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-04 15:20:36 +0530

    logic revise - sunday catch

---
+++ GreedyAlgorithms/MinDaysBuyToSurvive.java
@@ -0,0 +1,31 @@
+
+// revise logic 
+
+class Solution {
+    static int minimumDays(int S, int N, int M) {
+        // code here
+
+        int sunday = S / 7;
+
+        int bd = S - sunday;
+
+        int tf = S * M;
+
+        int ans = 0;
+
+        if (tf % N == 0) {
+
+            ans = tf / N;
+        } else {
+
+            ans = tf / N + 1;
+        }
+
+        if (ans <= bd)
+            return ans;
+
+        else
+            return -1;
+
+    }
+}
