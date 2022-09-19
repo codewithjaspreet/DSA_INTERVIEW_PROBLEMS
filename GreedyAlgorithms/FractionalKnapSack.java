ee7572d8162d2e6c3f2de100751d58f859deacf9
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-19 11:20:02 +0530

    comparator use , greedy

---
+++ GreedyAlgorithms/FractionalKnapSack.java
@@ -0,0 +1,53 @@
+
+/*
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+*/
+
+// fractional knapsack
+// time complexity: O(nlogn) space complexity: O(1)
+// approach: greedy approach
+class itemComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item a, Item b) {
+        double r1 = (double) (a.value) / (double) (a.weight);
+        double r2 = (double) (b.value) / (double) (b.weight);
+        if (r1 < r2)
+            return 1;
+        else if (r1 > r2)
+            return -1;
+        else
+            return 0;
+    }
+}
+
+class Solution {
+    double fractionalKnapsack(int W, Item arr[], int n) {
+        Arrays.sort(arr, new itemComparator());
+
+        int curWeight = 0;
+        double finalvalue = 0.0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (curWeight + arr[i].weight <= W) {
+                curWeight += arr[i].weight;
+                finalvalue += arr[i].value;
+            }
+
+            else {
+                int remain = W - curWeight;
+                finalvalue += ((double) arr[i].value / (double) arr[i].weight) * (double) remain;
+                break;
+            }
+        }
+
+        return finalvalue;
+
+    }
+}
