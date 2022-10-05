efc6c1c3ed7b12e3457cdc42fd792d15ccf01d3a
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-10-05 16:37:08 +0530

    two pointer ,important

--- Arrays/Easy/RemoveDuplicatesFromSortedArray.java
+++ Arrays/Easy/RemoveDuplicatesFromSortedArray.java
@@ -1,3 +1,39 @@
-public class RemoveDuplicatesFromSortedArray {
 
+// brute force , time complexity O(nlogn) , space complexity O(n)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        int j = 0; // Here j represents at which index the next distinct element needs to be placed
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return set.size();
+    }
+}
+
+// two pointer approach , time complexity O(n) , space complexity O(1)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+
+    }
 }
