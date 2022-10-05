b6f40a4ba6e7bd230beb2679389f92cee71a3418
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-10-05 18:04:09 +0530

    3 step algo

--- Arrays/Easy/LeftRotateArrayByOnePlace.java
+++ Arrays/Easy/LeftRotateArrayByOnePlace.java
@@ -1,3 +1,40 @@
-public class LeftRotateArrayByOnePlace {
+
+// part 1 reverse the part of array from 0 to len- k -1
+// part 2 reverse the part of array from len - k to len - 1
+// part 3 reverse the whole array
+
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        if (k < 0)
+            k += nums.length;
+
+        reverse(0, nums.length - k - 1, nums);
+
+        reverse(nums.length - k, nums.length - 1, nums);
+
+        reverse(0, nums.length - 1, nums);
+
+    }
+
+    static void reverse(int i, int j, int[] nums) {
+
+        int left = i;
+        int right = j;
+
+        while (left < right) {
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+
+    }
 
 }
