311f79135371a9f643fba15c2131e9febecd3b0e
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-10-05 19:56:54 +0530

    easy peasy

---
+++ Arrays/Easy/MaxConsecutivesOnes.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                result = Math.max(count, result);
+            } else
+                count = 0;
+        }
+
+        return result;
+    }
+}
