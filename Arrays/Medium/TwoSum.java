deafee376869c124a9c6721f0751c7998052babc
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-10-06 13:55:31 +0530

    brute & optimised

--- Arrays/Medium/TwoSum.java
+++ Arrays/Medium/TwoSum.java
@@ -1,5 +1,34 @@
 // brute force , time complexity O(n^2) , space complexity O(1)
 
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] ans = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+
+                    ans[0] = i;
+
+                    ans[1] = j;
+
+                    return ans;
+
+                }
+
+            }
+
+        }
+
+        return ans;
+
+    }
+}
+
 // optimized , time complexity O(n) , space complexity O(n)
 class Solution {
     public int[] twoSum(int nums[], int target) {
