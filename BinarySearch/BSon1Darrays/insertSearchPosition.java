61ea33e7c00f93db27e951a85678f59438a04392
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-10-08 12:19:40 +0530

    bs 1d arrays appliation

---
+++ BinarySearch/BSon1Darrays/insertSearchPosition.java
@@ -0,0 +1,32 @@
+// binary search on 1D arrays
+
+// logn time complexity
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] > target) {
+
+                high = mid - 1;
+            }
+
+            else {
+
+                low = mid + 1;
+            }
+
+        }
+
+        return low;
+    }
+}
