b21a7991ec90efbf05b6a611c3ebda86b2e3cfb3
Jaspreet Singh <85099922+codewithjaspreet@users.noreply.github.com>
2022-11-02 12:15:14 +0530

    Update BoundaryTraversal.java

--- Trees/BoundaryTraversal.java
+++ Trees/BoundaryTraversal.java
@@ -7,81 +7,60 @@
 
 // time - O(H)
 
-class Solution {
-    ArrayList<Integer> boundary(Node node) {
+class Solution
+{
+    
+    static void traverseLeft(Node node , ArrayList<Integer> ans){
+        
+        if(node == null || (node.left == null && node.right == null)) return;
 
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        if (isLeaf(node) == false)
-            ans.add(node.data);
-
-        addLeftBoundary(node, ans);
-        addLeaves(node, ans);
-        addRightBoundary(node, ans);
-
-        return ans;
-
+        ans.add(node.data);
+        if(node.left != null) traverseLeft(node.left,ans);
+        else traverseLeft(node.right,ans);
     }
-
-    public static boolean isLeaf(Node node) {
-
-        if (node.right == null && node.left == null)
-            return true;
-        return false;
+    static void leafTraverse(Node node , ArrayList<Integer> ans){
+        
+        if(node== null ) return;
+        if(node.left == null && node.right == null){
+           ans.add(node.data);
+           return;
+        } 
+        
+        leafTraverse(node.left  , ans);
+        leafTraverse(node.right , ans);
+        
     }
-
-    public static void addLeftBoundary(Node node, ArrayList<Integer> ans) {
-
-        Node cur = node.left;
-
-        if (cur != null) {
-
-            if (isLeaf(cur) == false)
-                ans.add(cur.data);
-
-            if (cur.left != null)
-                cur = cur.left;
-
-            else
-                cur = cur.right;
-        }
+    
+    static void rightTraverse(Node node,  ArrayList<Integer > ans){
+        
+         if(node == null || (node.left == null && node.right == null)) return;
+         
+         if(node.right != null) rightTraverse(node.right , ans);
+         
+         
+         else rightTraverse(node.left  , ans);
+         
+         ans.add(node.data);
+         
     }
-
-    public static void addRightBoundary(Node node, ArrayList<Integer> ans) {
-
-        Node cur = node.right;
-        ArrayList<Integer> temp = new ArrayList<>();
-
-        if (cur != null) {
-
-            if (isLeaf(cur) == false)
-                ans.add(cur.data);
-
-            if (cur.right != null)
-                cur = cur.right;
-
-            else
-                cur = cur.right;
-        }
-        int i;
-
-        for (i = temp.size() - 1; i >= 0; i--) {
-
-            ans.add(temp.get(i));
-        }
-    }
-
-    public static void addLeaves(Node root, ArrayList<Integer> ans) {
-
-        if (isLeaf(root)) {
-            ans.add(root.data);
-            return;
-        }
-
-        if (root.left != null)
-            addLeaves(root.left, ans);
-        if (root.right != null)
-            addLeaves(root.right, ans);
-    }
-
+	ArrayList <Integer> boundary(Node root)
+	{
+	    ArrayList<Integer> ans = new ArrayList<>();
+	    
+	    if(root == null) return ans;
+	    
+	    ans.add(root.data);
+	    
+	    traverseLeft(root.left , ans);
+	    
+	    leafTraverse(root.left , ans);
+	    
+	    leafTraverse(root.right , ans);
+	    
+	    rightTraverse(root.right   , ans);
+	    
+	    
+	    return ans;
+	}
 }
+
