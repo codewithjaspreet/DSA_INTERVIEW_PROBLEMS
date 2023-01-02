ef3612e9c120a1bc30a6dad018a493f803857fd6
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-01-02 21:48:15 +0530

    bruteadded-tommorrow

---
+++ LinkedList/MediumProblemsOnLL/LLcycleStarting.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+
+public class LLcycleStarting {
+
+    static class Node {
+
+        Node next;
+        int data;
+
+        public Node(Node next, int data) {
+            this.next = next;
+            this.data = data;
+        }
+
+        // brute force with o(n) space and o(n) time
+
+        static Node isCycle(Node node) {
+
+            HashSet<Node> set = new HashSet<>();
+
+            while (node != null) {
+
+                if (set.contains(node))
+                    return node;
+
+                else {
+                    set.add(node);
+                    node = node.next;
+                }
+            }
+
+            return null;
+
+        }
+
+        public boolean hasCycle(Node head) {
+            if (head == null) {
+                return false;
+            }
+            Node slow = head, fast = head;
+
+            while (fast.next != null && fast.next.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
