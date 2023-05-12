8dab9cdba5377d8bb9881fe38fa7ab3a25f72af8
Jaspreet Singh <jaspreetsodhi02@gmail.com>
2023-05-12 11:41:39 +0530

    Update First negative integer in every window of size k.java

--- SlidingWindow/FixedSizeWindow/First negative integer in every window of size k.java
+++ SlidingWindow/FixedSizeWindow/First negative integer in every window of size k.java
@@ -1,40 +1,45 @@
-// Expected Time Complexity: O(N)
-// Expected Auxiliary Space: O(K)
+import java.util.*;
+public class Solution {
+	public static int[] firstNegative(int[] arr, int n, int k) {
+        // Write your code here.
 
-class Compute {
+		int[] ans = new int[n-k +1];
+		int ind = 0;
+		int i  = 0 ; int j = 0;
 
-    public long[] printFirstNegativeInteger(long A[], int N, int K) {
+		    Queue<Integer> q = new LinkedList<>();
 
-        ArrayList<Long> ans = new ArrayList<>();
-        Deque<Long> q = new ArrayDeque<>();
-        int left = 0;
-        int right = 0;
-        while (right < N) {
-            // calculations
-            if (A[right] < 0) {
-                q.add(A[right]);
-            }
-            if (right - left + 1 < K) {
-                right++;
-            } else if (right - left + 1 == K) {
-                // take ans from calculations and remove from queue
-                if (q.isEmpty()) {
-                    ans.add((long) 0); // no negative number found in window
-                } else {
-                    ans.add(q.peek());
-                    // sliding the window
-                    if (A[left] == q.peek()) {
-                        q.poll();
-                    }
+    while (j < n) {
+        // If current element is negative, add it to the queue
+        if (arr[j] < 0) {
+            q.offer(arr[j]);
+        }
+
+        // If the window size is not reached yet, increment j
+        if (j - i + 1 < k) {
+            j++;
+        } 
+        else { // Window size is reached
+
+            // If the queue is empty, there are no negative numbers in the window
+            if (q.isEmpty()) {
+                ans[ind] = 0;
+            } 
+            else { // Queue is not empty, so the first negative number in the window is the head of the queue
+                ans[ind] = q.peek();
+                // Remove the head of the queue if it is outside the current window
+                if (arr[i] == q.peek()) {
+                    q.poll();
                 }
-                right++;
-                left++;
             }
+
+            ind++;
+            i++;
+            j++;
         }
-        long[] a = new long[ans.size()];
-        for (int k = 0; k < ans.size(); k++) {
-            a[k] = ans.get(k);
-        }
-        return a;
     }
+
+    return ans;
+}
+
 }
