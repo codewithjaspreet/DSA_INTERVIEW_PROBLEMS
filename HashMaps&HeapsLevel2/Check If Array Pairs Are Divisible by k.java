1d2f3b80cda8f80e919faf688c067c0a457b1bd2
Jaspreet Singh <jaspreetsodhi02@gmail.com>
2023-05-14 14:16:27 +0530

    Update Check If Array Pairs Are Divisible by k.java

--- HashMaps&HeapsLevel2/Check If Array Pairs Are Divisible by k.java
+++ HashMaps&HeapsLevel2/Check If Array Pairs Are Divisible by k.java
@@ -1,20 +1,44 @@
-//  x - k -x
-// k /2 -  even
-// 0 - even
 
-class Solution{ 
-    public boolean canArrange(int[] arr, int k) {
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int n : arr) {
-                int rem = n % k;
-                if (rem < 0) rem += k;
-                map.put(rem, map.getOrDefault(rem, 0) + 1);
-            }
-            for (int key : map.keySet()) {
-                if (key == k - key || key == 0) {if (map.get(key) % 2 != 0) return false;}
-                else if (!map.get(key).equals(map.getOrDefault(k - key, 0))) return false;  // can not use == here, Integer boxed;
-            }
-            return true;
+class Solution {
+    public boolean canArrange(int[] arr, int k) 
+    {
+        if(arr.length % 2 != 0)
+            return false;
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<arr.length; i++)
+        {
+            int rem = arr[i] % k;
+            if(rem < 0)
+                rem = rem + k;
+            
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
         }
         
+        for(Integer i: map.keySet())
+        {
+            if(i == 0)
+            {
+                if(map.get(i) % 2 !=0)
+                    return false;
+            }
+            
+            else if (i * 2 == k)
+            {
+                if(map.get(i) % 2 != 0)
+                    return false;
+            }
+            
+            else
+            {
+               int c1 = map.get(i);
+               int c2 = map.getOrDefault(k-i, 0); 
+                
+               if(c1 != c2)
+                   return false;
+            }
+        }
+        return true;
     }
+}
