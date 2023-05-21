1f65062831742dbd6f698485bbc3bf9f5edd8129
jaspreetsinghsodhi <jaspreetsodhi02@gmail.com>
2023-05-21 11:57:41 +0530

    st-7

---
+++ Stacks&Queues/redundant_brackets.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+public class redundant_brackets {
+    
+
+    public static int checkRedundancy(String s) {
+        // code here
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            
+            if(c == '(' || c == '+' || c == '-' || c == '*' || c == '/') st.push(c);
+            
+            else{
+                
+               if(c == ')'){
+                   
+                   boolean isRed = true;
+                   
+                   while(st.peek() != '('){
+                       
+                       char ch  = st.peek();
+                       
+                       if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+                           
+                           isRed = false;
+                       }
+                       
+                       st.pop();
+                   }
+                   
+                   
+                   if(isRed == true) return 1;
+                   st.pop();
+                   
+                   
+                }
+               }
+            }
+            
+            return 0;
+        }
+        
+        
+    }
+
+        
+
