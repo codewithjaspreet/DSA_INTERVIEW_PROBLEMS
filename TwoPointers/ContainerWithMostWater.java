019b4f3ba9e785b92c0c31e6e1f9a043ca675ab1
jaspreetsinghsodhi <jaspreetsodhi02@gmail.com>
2023-05-23 10:12:29 +0530

    opti

--- TwoPointers/ContainerWithMostWater.java
+++ TwoPointers/ContainerWithMostWater.java
@@ -19,7 +19,31 @@ public class ContainerWithMostWater {
         return maxArea;
     }
 
+
+    // optimised
+
+        public int maxAreaOptimised(int[] height) {
+            
+    int len = height.length;
+            int left = 0;
+            int right = len - 1;
+            int max = Math.min(height[left], height[right]) * (right - left);
+            while (left < right) {
+                // Move the shorter lines each time
+                if (height[left] <= height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
     
+                // update the max area
+                max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            }
+    
+            return max;
+        }
+
+
 
 
 }
